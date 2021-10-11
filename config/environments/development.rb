5ad18a6fe9f6280f08ece7ba847e09dc50e2f2a8
Yashwanth <yashvanthyashujnvt@gmail.com>
2021-10-11 12:33:19 +0530

    email setup

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -10,6 +10,8 @@ Rails.application.configure do
 
   # Do not eager load code on boot.
   config.eager_load = false
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { :host => 'yashwanth-saas-app.herokuapp.com', :protocol => 'https'}
 
   # Show full error reports.
   config.consider_all_requests_local = true
